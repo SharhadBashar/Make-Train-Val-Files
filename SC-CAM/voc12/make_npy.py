9acc368265019e341702035a79c3b33a066887ae
Sharhad Bashar <sbashar@montrium.com>
2021-11-10 01:33:19 -0500

    new

--- SC-CAM/voc12/make_npy.py
+++ SC-CAM/voc12/make_npy.py
@@ -13,9 +13,21 @@ import numpy as np
 # np.save('train_label_all.npy', arr)
 
 files = np.load('train_label_all.npy', allow_pickle = True)
+print(files.shape)
 print(type(files))
 print(files[24])
 
 files = np.load('train_label.npy', allow_pickle = True)
 print(type(files))
 print(files[24])
+
+# temp = []
+# files = np.load('cls_labels.npy', allow_pickle = True).item()
+# with open('train_aug.txt', 'r') as lines:
+#   for line in lines:
+#     image, _ = line.strip('\n').split(' ')
+#     img = image.split('/')[-1].split('.')[0]
+#     temp.append(files['' + img])
+
+# arr = np.asarray(temp)
+# np.save('train_label_all.npy', arr)
